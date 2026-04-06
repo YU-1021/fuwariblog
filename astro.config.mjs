7ce9bf91697272dbcc34239785006823515a6ce8
Yu <2848476289@qq.com>
2026-04-06 11:27:46 +0800

    Revert "更新 astro.config.mjs"
    This reverts commit f81f4bb9365158267ba5ad0c791089e62cd7931a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://yuuu.top/",
+	site: "https://fuwari.vercel.app/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
